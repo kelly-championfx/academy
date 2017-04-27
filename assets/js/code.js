c44b58ce7b339af041e4c35ab43bca49bf5ec6cb
Sash <sash@binary.com>
2017-04-27 22:06:08 +0300

    acc

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,87 +1,8 @@
-$(function() {
-    var slider = function() {
-        var slider = $('#slider');
-        if (slider.size()) {
-            $('.invisibleslides').show();
-            slider.slides({
-                container: 'slides-container',
-                paginationClass: 'slides-pagination',
-                generatePagination: false,
-                generateNextPrev: false,
-                start: 1,
-                preload: true,
-                play: 10000,
-                slideSpeed: 800,
-            });
-            if ($('.slides_control').height() < 300) {
-                $('.slides_control').css('min-height', '300px');
-            }
-        }
-    };
-    slider();
+$(function() { 
     var currLang = document.documentElement.lang;
     $('#language_select select').on('change', function() {
         var lang = $(this).val();
         window.location.href = '/' + (lang === 'en' ? '' : lang);
-    });
-    $(".nav-button").click(function() {
-        $(".nav-button,.primary-nav").toggleClass("open");
-    });
-    ! function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-    }(document, "script", "twitter-wjs");
-    $('.wcustomhtml').css('overflow', 'visible');
-
-    function getDailyReport(media, prefix) {
-        var options = {
-            prefix: prefix,
-            media: media,
-            campaign: '1',
-            mode: 'txt'
-        };
-        $.ajax({
-            type: 'GET',
-            url: '//js.binary.com/javascript.php',
-            data: options,
-            dataType: 'xml',
-            success: function(xml) {
-                $(xml).find('item').each(function(idx) {
-                    var title = $(this).find('title:first').text().replace(/regentmarkets\d.*$/, ''),
-                        pubDate = $(this).find('pubDate').text().replace(/\+0000$/, 'GMT');
-                    var post = $(this).find('content\\:encoded').text();
-                    if (!post) {
-                        post = $(this).find('encoded').text();
-                    }
-                    post = post.replace(']]&gt;', '').replace(']]>', '');
-                    $('.report-list').append($('<option>', {
-                        value: idx,
-                        text: title
-                    }));
-                    $('<div id="report-' + idx + '" class="single-report">').append('<h2>' + title + '</h2>').append('<p>' + post + '</p>').toggle(idx === 0).appendTo('.daily-report');
-                });
-            },
-            jsonp: 'jsonp'
-        });
-    }
-    if ($('.daily-report').length) {
-        if (currLang === 'id') {
-            getDailyReport('876', '8grqRDVc105iX6ztb0GwqWNd7ZgqdRLk');
-        } else if (currLang === 'ru') {
-            getDailyReport('875', 'bPzDzniJKAKx76XffYA0JmNd7ZgqdRLk');
-        } else {
-            getDailyReport('26', 'bPzDzniJKAJHH6eEtUVc2GNd7ZgqdRLk');
-        }
-    };
-    $('.report-list').on('change', function() {
-        $('.single-report').hide();
-        $('#report-' + $('.report-list').val()).show();
     });
     $('div[data-role=youtube-playlist]').each(function() {
         var $playlist = this,
@@ -104,7 +25,6 @@ $(function() {
             $(thumbs.join('')).appendTo($playlist);
         }
     });
-
     function isPublic(value) {
         return (value.status.privacyStatus === 'public');
     }
